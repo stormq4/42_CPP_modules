fa9399ec95f382432ae0f199d8f8193af6e9a372
Storm De quaij <sde-quai@f1r4s5.codam.nl>
2022-08-23 16:42:43 +0200

    started M7

---
+++ M_07/ex00/Int.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Int.hpp                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sde-quai <sde-quai@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/23 15:50:52 by sde-quai      #+#    #+#                 */
+/*   Updated: 2022/08/23 15:52:17 by sde-quai      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INT_HPP
+# define INT_HPP
+
+#include <iostream>
+#include <string>
+
+template <class T>
+class Int {
+	
+}
+
+#endif
