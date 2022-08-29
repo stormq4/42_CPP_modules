a1c348c60bc75df4da7787fe1026310bb1c08e69
Storm De quaij <sde-quai@f1r4s1.codam.nl>
2022-08-29 10:26:51 +0200

    fixed main.cpp

--- M_07/ex02/main.cpp
+++ M_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-quai <sde-quai@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/24 11:54:59 by sde-quai      #+#    #+#                 */
-/*   Updated: 2022/08/24 16:52:18 by sde-quai      ########   odam.nl         */
+/*   Updated: 2022/08/29 10:23:38 by sde-quai      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,92 @@
 #include <string>
 #include <iostream>
 
+#define MAX_VAL 750
+
 int	main() {
-	try {
-		Array<int> arr(50);
-		
-		for (int i = 0; i < arr.size(); i++) {
-			arr[i] = i * i;
+	{
+		try {
+			Array<int> arr(50);
+			
+			for (int i = 0; i < arr.size(); i++) {
+				arr[i] = i * i;
+			}
+			for (int i = 0; i < arr.size(); i++) {
+				std::cout << i << " --> " << arr[i] << std::endl;
+			}
+			arr[51];
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
 		}
-		for (int i = 0; i < arr.size(); i++) {
-			std::cout << i << " --> " << arr[i] << std::endl;
-		}
-		arr[51];
-	} catch (const std::exception &e) {
-		std::cout << e.what() << std::endl;
 	}
 	std::cout << "\n------------------------\n";
-	try	{
-		Array<std::string> arr(20);
-		
-		for (int i = 0; i < arr.size(); i++) {
-			arr[i] = "halloooo";
+	{
+		try	{
+			Array<std::string> arr(20);
+			
+			for (int i = 0; i < arr.size(); i++) {
+				arr[i] = "halloooo";
+			}
+			for (int i = 0; i < arr.size(); i++) {
+				std::cout << i << " --> " << arr[i] << std::endl;
+			}
+			arr[-1]; // should fail
+			Array<std::string> copyArray = arr;
+			for (int i = 0; i < copyArray.size(); i++) {
+				std::cout << i << " --> " << copyArray[i] << std::endl;
+			}
+		} catch(const std::exception& e) {
+			std::cout << e.what() << std::endl;
 		}
-		for (int i = 0; i < arr.size(); i++) {
-			std::cout << i << " --> " << arr[i] << std::endl;
+	}
+	std::cout << "\n\n------------------------\n";
+	std::cout << "\n--------main.cpp in subject-------\n";
+	{
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
 		}
-		arr[-1];
-		Array<std::string> copyArray = arr;
-		for (int i = 0; i < copyArray.size(); i++) {
-			std::cout << i << " --> " << copyArray[i] << std::endl;
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
 		}
-	} catch(const std::exception& e) {
-		std::cout << e.what() << std::endl;
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
 	}
 	return 0;
 }
